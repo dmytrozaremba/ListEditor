995b2926ff0a157d8762db246ae3de6116fcfbfd
Dmytro Zaremba <dmytrozaremba@gmail.com>
2020-04-15 22:21:01 +0300

    Visual design updates

---
+++ ListEditor/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ListEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle25 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle26 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle27 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvBookList = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelFilter = new System.Windows.Forms.Panel();
+            this.pictureBoxFilter = new System.Windows.Forms.PictureBox();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookList)).BeginInit();
+            this.panelMenu.SuspendLayout();
+            this.panelFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFilter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBookList
+            // 
+            this.dgvBookList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvBookList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvBookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvBookList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBookList.Location = new System.Drawing.Point(0, 45);
+            this.dgvBookList.Margin = new System.Windows.Forms.Padding(10);
+            this.dgvBookList.Name = "dgvBookList";
+            this.dgvBookList.RowHeadersWidth = 40;
+            this.dgvBookList.Size = new System.Drawing.Size(664, 296);
+            this.dgvBookList.TabIndex = 0;
+            this.dgvBookList.TabStop = false;
+            this.dgvBookList.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBookList_CellEndEdit);
+            this.dgvBookList.CellParsing += new System.Windows.Forms.DataGridViewCellParsingEventHandler(this.dgvBookList_CellParsing);
+            this.dgvBookList.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dgvBookList_CellValidating);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Author Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Title";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle25.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle25.NullValue = null;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle25;
+            this.Column3.HeaderText = "Year of Publishing";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle26.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle26;
+            this.Column4.HeaderText = "Number of Pages";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle27.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Column5.DefaultCellStyle = dataGridViewCellStyle27;
+            this.Column5.HeaderText = "Price";
+            this.Column5.Name = "Column5";
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Controls.Add(this.panelFilter);
+            this.panelMenu.Controls.Add(this.buttonSaveAs);
+            this.panelMenu.Controls.Add(this.buttonSave);
+            this.panelMenu.Controls.Add(this.buttonOpen);
+            this.panelMenu.Controls.Add(this.buttonNew);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(664, 45);
+            this.panelMenu.TabIndex = 4;
+            // 
+            // panelFilter
+            // 
+            this.panelFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelFilter.BackColor = System.Drawing.SystemColors.Window;
+            this.panelFilter.Controls.Add(this.pictureBoxFilter);
+            this.panelFilter.Controls.Add(this.textBoxFilter);
+            this.panelFilter.Location = new System.Drawing.Point(480, 10);
+            this.panelFilter.Name = "panelFilter";
+            this.panelFilter.Size = new System.Drawing.Size(176, 26);
+            this.panelFilter.TabIndex = 10;
+            this.toolTip.SetToolTip(this.panelFilter, "Filter");
+            this.panelFilter.Click += new System.EventHandler(this.panelFilter_Click);
+            // 
+            // pictureBoxFilter
+            // 
+            this.pictureBoxFilter.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBoxFilter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxFilter.Image = global::ListEditor.Properties.Resources.icons8_filter_32;
+            this.pictureBoxFilter.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxFilter.Name = "pictureBoxFilter";
+            this.pictureBoxFilter.Size = new System.Drawing.Size(20, 20);
+            this.pictureBoxFilter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxFilter.TabIndex = 6;
+            this.pictureBoxFilter.TabStop = false;
+            this.toolTip.SetToolTip(this.pictureBoxFilter, "Filter");
+            this.pictureBoxFilter.Click += new System.EventHandler(this.pictureBoxFilter_Click);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFilter.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFilter.Location = new System.Drawing.Point(29, 6);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(144, 13);
+            this.textBoxFilter.TabIndex = 5;
+            this.toolTip.SetToolTip(this.textBoxFilter, "Filter");
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.BackgroundImage = global::ListEditor.Properties.Resources.icons8_save_as_32;
+            this.buttonSaveAs.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSaveAs.Location = new System.Drawing.Point(124, 6);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(32, 32);
+            this.buttonSaveAs.TabIndex = 9;
+            this.toolTip.SetToolTip(this.buttonSaveAs, "Save File As...");
+            this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackgroundImage = global::ListEditor.Properties.Resources.icons8_save_32;
+            this.buttonSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSave.Location = new System.Drawing.Point(86, 6);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(32, 32);
+            this.buttonSave.TabIndex = 8;
+            this.toolTip.SetToolTip(this.buttonSave, "Save File");
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackgroundImage = global::ListEditor.Properties.Resources.icons8_opened_folder_32;
+            this.buttonOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonOpen.Location = new System.Drawing.Point(48, 6);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(32, 32);
+            this.buttonOpen.TabIndex = 7;
+            this.toolTip.SetToolTip(this.buttonOpen, "Open File...");
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.BackgroundImage = global::ListEditor.Properties.Resources.icons8_file_32;
+            this.buttonNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonNew.Location = new System.Drawing.Point(10, 6);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(32, 32);
+            this.buttonNew.TabIndex = 6;
+            this.toolTip.SetToolTip(this.buttonNew, "New File");
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 341);
+            this.Controls.Add(this.dgvBookList);
+            this.Controls.Add(this.panelMenu);
+            this.MinimumSize = new System.Drawing.Size(450, 380);
+            this.Name = "MainForm";
+            this.Text = "List Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookList)).EndInit();
+            this.panelMenu.ResumeLayout(false);
+            this.panelFilter.ResumeLayout(false);
+            this.panelFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFilter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBookList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Panel panelFilter;
+        private System.Windows.Forms.PictureBox pictureBoxFilter;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ ListEditor/MainForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ListEditor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        string fileName = string.Empty;
+        bool isSaved = true;
+
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (!isSaved)
+            {
+                var result = MessageBox.Show("Do you want to save changes?", "List Editor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    Save();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    dgvBookList.Rows.Clear();
+                    fileName = openFileDialog.FileName;
+                    string[] lines = File.ReadAllLines(fileName);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        dgvBookList.Rows.Add();
+                        string[] line = lines[i].Split(',').Select(s => s.Trim()).ToArray();
+                        for (int j = 0; j < line.Length; j++)
+                        {
+                            var stringValue = line[j];
+                            var cell = dgvBookList[j, i];
+                            switch (j)
+                            {
+                                case 2:
+                                case 3:
+                                    cell.Value = (stringValue == string.Empty) ? (int?)null : int.Parse(stringValue);
+                                    break;
+                                case 4:
+                                    cell.Value = (stringValue == string.Empty) ? (double?)null : double.Parse(stringValue, CultureInfo.InvariantCulture);
+                                    break;
+                                default:
+                                    cell.Value = stringValue;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Save()
+        {
+            List<string> lines = new List<string>();
+
+            for (int i = 0; i < dgvBookList.RowCount - 1; i++)
+            {
+                var line = string.Empty;
+
+                for (int j = 0; j < dgvBookList.Rows[i].Cells.Count; j++)
+                {
+                    string strValue;
+
+                    var cell = dgvBookList[j, i];
+
+                    if (cell.Value == null || cell.Value is string && string.Empty == ((string)cell.Value))
+                    {
+                        strValue = string.Empty;
+                    }
+                    else
+                    {
+                        switch (j)
+                        {
+                            case 2:
+                            case 3:
+                                strValue = ((int?)cell.Value)?.ToString() ?? string.Empty;
+                                break;
+                            case 4:
+                                strValue = ((double?)cell.Value)?.ToString(CultureInfo.InvariantCulture) ?? string.Empty;
+                                break;
+                            default:
+                                strValue = (string)cell.Value;
+                                break;
+                        }
+                    }
+
+                    line = string.Concat(line, (line == string.Empty ? string.Empty : ","), strValue);
+                }
+
+                lines.Add(line);
+            }
+
+            File.WriteAllLines(fileName, lines);
+            isSaved = true;
+        }
+
+        private void SaveAs()
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = saveFileDialog.FileName;
+                    Save();
+                }
+            }
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+            var searchTerm = textBoxFilter.Text;
+            for (int i = 0; i < dgvBookList.RowCount - 1; i++)
+            {
+                var matchesSearchTerm = false;
+                for (int j = 0; j < dgvBookList.Rows[i].Cells.Count; j++)
+                {
+                    var stringValue = dgvBookList.Rows[i].Cells[j].Value?.ToString() ?? string.Empty;
+                    if (stringValue.Contains(searchTerm))
+                    {
+                        matchesSearchTerm = true;
+                        break;
+                    }
+                }
+                dgvBookList.Rows[i].Visible = matchesSearchTerm;
+            }
+        }
+
+        private void dgvBookList_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            string errorText = string.Empty;
+
+            var stringValue = e.FormattedValue.ToString();
+
+            switch (e.ColumnIndex)
+            {
+                case 2:
+                case 3:
+                    if (stringValue != string.Empty && !int.TryParse(stringValue, out int intValue))
+                    {
+                        errorText = string.Format("{0} must be an integer", dgvBookList.Columns[e.ColumnIndex].HeaderText);
+                    }
+                    break;
+                case 4:
+                    if (stringValue != string.Empty && !double.TryParse(stringValue.Replace(',', '.'), NumberStyles.Number, CultureInfo.InvariantCulture, out double doubleValue))
+                    {
+                        errorText = "Price must be a double";
+                    }
+                    break;
+            }
+
+            if (errorText != string.Empty)
+            {
+                dgvBookList.Rows[e.RowIndex].ErrorText = errorText;
+                e.Cancel = true;
+
+                MessageBox.Show(errorText, "Data Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                isSaved = false;
+            }
+
+            Console.WriteLine($"Validating [{e.ColumnIndex},{e.RowIndex}]: {e.FormattedValue}");
+        }
+
+        private void dgvBookList_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvBookList.Rows[e.RowIndex].ErrorText = string.Empty;
+        }
+
+        private void dgvBookList_CellParsing(object sender, DataGridViewCellParsingEventArgs e)
+        {
+            var stringValue = e.Value.ToString();
+
+            switch (e.ColumnIndex)
+            {
+                case 2:
+                case 3:
+                    e.Value = (stringValue == string.Empty) ? (int?)null : int.Parse(stringValue);
+                    e.ParsingApplied = true;
+
+                    break;
+                case 4:
+                    e.Value = (stringValue == string.Empty) ? (double?)null : double.Parse(stringValue, CultureInfo.InvariantCulture);
+                    e.ParsingApplied = true;
+
+                    break;
+            }
+
+            Console.WriteLine($"Parsing({e.ParsingApplied}) [{e.ColumnIndex},{e.RowIndex}]: {e.Value} {(e.Value == null ? "null" : e.Value.GetType().Name)}");
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            if (!isSaved)
+            {
+                var result = MessageBox.Show("Do you want to save changes?", "List Editor", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    Save();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            dgvBookList.Rows.Clear();
+            isSaved = true;
+        }
+
+        private void pictureBoxFilter_Click(object sender, EventArgs e)
+        {
+            textBoxFilter.Focus();
+        }
+
+        private void panelFilter_Click(object sender, EventArgs e)
+        {
+            textBoxFilter.Focus();
+        }
+    }
+}
